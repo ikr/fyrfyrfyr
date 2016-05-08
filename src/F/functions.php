b905cd6dfd805a1c3503fd0a164796dcde3d74bf
Ivan Krechetov <ikr@ikr.su>
2016-05-08 21:18:01 +0200

    A first working curry()

--- src/F/functions.php
+++ src/F/functions.php
@@ -2,7 +2,26 @@
 
 namespace F;
 
-function curry()
+function curry($f)
 {
+    $accumulate = function (array $appliedArgs, $totalArgsCount) use ($f, &$accumulate)
+    {
+        if (count($appliedArgs) >= $totalArgsCount) {
+            return call_user_func_array($f, $appliedArgs);
+        }
 
+        return function ($x) use (&$accumulate, $appliedArgs, $totalArgsCount)
+        {
+            $append = function ($item, array $list)
+            {
+                $list[] = $item;
+                return $list;
+            };
+
+            return $accumulate($append($x, $appliedArgs), $totalArgsCount);
+        };
+    };
+
+    $meta = new \ReflectionFunction($f);
+    return $accumulate([], $meta->getNumberOfParameters());
 }
--- tests/CurryTest.php
+++ tests/CurryTest.php
@@ -2,8 +2,32 @@
 
 class CurryTest extends PHPUnit_Framework_TestCase
 {
-    public function testNothing()
+    public function testCurriedAdditionOnTwo()
     {
-        $this->assertNull(F\curry());
+        $add = F\curry(
+            function ($a, $b)
+            {
+                return $a + $b;
+            }
+        );
+
+        $add10 = $add(10);
+
+        $this->assertSame(13, $add10(3));
+    }
+
+    public function testCurriedAdditionOnThree()
+    {
+        $add = F\curry(
+            function ($a, $b, $c)
+            {
+                return $a + $b + $c;
+            }
+        );
+
+        $add1 = $add(1);
+        $add12 = $add1(2);
+
+        $this->assertSame(6, $add12(3));
     }
 }
