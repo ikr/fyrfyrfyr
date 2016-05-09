cffd0411c33ed21a35d79ccd00e653ed746cc2bd
Ivan Krechetov <ikr@ikr.su>
2016-05-09 15:25:17 +0200

    A composition test with text

--- tests/ComposeTest.php
+++ tests/ComposeTest.php
@@ -10,4 +10,12 @@ class ComposeTest extends PHPUnit_Framework_TestCase
         $f = F\compose([$inc, $negate, 'pow']);
         $this->assertSame(-80, $f(3, 4));
     }
+
+    public function testSomeText()
+    {
+        $exclaim = function ($x) { return "$x!"; };
+        $shout = F\compose([$exclaim, 'strtoupper']);
+
+        $this->assertSame('Y U NO COMPOSE!', $shout('y u no compose'));
+    }
 }
