27debdd710ba7b2550b413e9fc936b9a59f71517
Ivan Krechetov <ikr@ikr.su>
2016-05-09 16:54:42 +0200

    Document the Maybe signatures

--- src/F/Maybe.php
+++ src/F/Maybe.php
@@ -11,16 +11,19 @@ class Maybe
         $this->value = $value;
     }
 
+    // isNothing :: Boolean
     public function isNothing()
     {
         return is_null($this->value);
     }
 
+    // of :: a -> Maybe a
     public static function of($value)
     {
         return new Maybe($value);
     }
 
+    // map :: (a -> b) -> Maybe b
     public function map($f)
     {
         return $this->isNothing() ? self::of(null) : self::of(call_user_func($f, $this->value));
